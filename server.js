059cd496461259a5d1bc9a093330f919a34457f6
dafgefsdvsdgwsegsegdgsdgdfgfdgdfgdfgjfg <gta5dynasty@gmail.com>
2025-11-06 18:44:41 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { Client, GatewayIntentBits } = require('discord.js');
+const app = express();
+const port = 3000;
+
+let bot;
+
+app.use(express.static('public')); // Stellt index.html bereit
+
+app.get('/start-bot', async (req, res) => {
+  if (bot) return res.send('Bot läuft bereits!');
+
+  bot = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+  bot.once('ready', () => console.log(`Bot angemeldet als ${bot.user.tag}`));
+
+  try {
+    await bot.login('MTQxMzQzOTE1Njk4OTk4NDg2OA.GUKtDM.o4WOgQF-lgqcKvrLR1kaEoAEgoh2it7-RhAQtM');
+    res.send('Bot gestartet!');
+  } catch (err) {
+    console.error(err);
+    res.send('Fehler beim Starten des Bots');
+  }
+});
+
+app.listen(port, () => console.log(`Server läuft auf http://localhost:${port}`));
